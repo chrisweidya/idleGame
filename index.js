ed0e97b73ffa81c8ffca92e9fc86c7f1c33e0ccc
chrisweidya <chrisweidya@hotmail.com>
2017-12-06 19:40:25 -0800

    test2

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainContainer from './src/mainContainer.js'
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
-});
-
 ReactDOM.render(
 	<MainContainer className="main-container">
 	</MainContainer>, 
